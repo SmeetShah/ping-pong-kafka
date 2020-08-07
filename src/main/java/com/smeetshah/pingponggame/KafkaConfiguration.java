8e95be7c2c8c17c607b866077530d8bc342fc8d4
Smeet Shah <smeetshh@gmail.com>
2020-08-07 14:34:34 -0400

    Docker Image Integration w/ docker compose

--- src/main/java/com/smeetshah/pingponggame/KafkaConfiguration.java
+++ src/main/java/com/smeetshah/pingponggame/KafkaConfiguration.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,10 +18,13 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfiguration {
 
+    @Value("${KAFKA_CONNECT}")
+    private String bootStrapServer;
+
     @Bean
     public ProducerFactory<String,String> producerFactory(){
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.1:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServer);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<String, String>(config);
@@ -34,7 +38,7 @@ public class KafkaConfiguration {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.1:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServer);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -47,11 +51,6 @@ public class KafkaConfiguration {
                 factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
-    }
-
-    @Bean
-    public NewTopic topic1() {
-        return new NewTopic("mytopic", 1, (short) 1);
     }
 
 }
